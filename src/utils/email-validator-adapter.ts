e3b14ec67fef5ea8f7b9629546a06e40cc13eb47
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-01-25 17:08:01 +0100

    feat: ensure EmailValidatorAdapter return true if validator returns true

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,7 +1,8 @@
-import {} from '../presentation/protocols/email-validator'
+import { EmailValidator } from '../presentation/protocols/email-validator'
+import validator from 'validator'
 
 export class EmailValidatorAdapter {
   isValid (email: string): boolean {
-    return false
+    return validator.isEmail(email)
   }
 }
